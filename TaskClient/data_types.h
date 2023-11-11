515ad35b3ba7f9e80d7e8594220d7cf9873e11f6
Oleg M <moleg_9432@rambler.ru>
2023-11-11 13:26:40 +0300

    In development

--- TaskClient/data_types.h
+++ TaskClient/data_types.h
@@ -4,10 +4,17 @@
 #pragma once
 
 enum class UserType {
-    Administrator,
-    Operator,
-    User,
-    Unknown
+    Administrator = 1,
+    Operator = 2,
+    User = 3
+};
+
+enum class TaskStatus {
+    NotAppointed = 1,
+    Appointed = 2,
+    InProgress = 3,
+    Rejected = 4,
+    Completed = 5
 };
 
 #endif // DATA_TYPES_H
