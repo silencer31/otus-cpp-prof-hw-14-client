53a6cc07aa5225ee3a238a171483e945ca48880c
Oleg M <moleg_9432@rambler.ru>
2024-01-28 22:09:36 +0300

    In development

--- TaskClient/Windows/user_functions.cpp
+++ TaskClient/Windows/user_functions.cpp
@@ -71,19 +71,221 @@ bool UserWindow::get_task_statuses()
 // Создать задачу для себя.
 void UserWindow::create_task()
 {
+    const QString task_name   = ui->leTaskName->text();
+    const QString deadline    = ui->leDeadLine->text();
+    const QString description = ui->leDescription->text();
 
+    // Проверка, заполнены ли поля.
+    if (task_name.isEmpty() || deadline.isEmpty() || description.isEmpty()) {
+        show_message(QString("All the fields have to be filled in!"));
+        return;
+    }
+
+    // Проверка, используется ли уже такое имя задачи.
+    if (data_keeper_ptr->task_name_received(task_name)) {
+        show_message(QString("Task name %1 is already in use!\nPlease enter another value").arg(task_name));
+        return;
+    }
+
+    lock_buttons();
+
+    // Отправляем запрос на создание новой задачи.
+    if ( !request_manager_ptr->send_add_task(own_id, deadline, task_name, description) ) {
+        show_message(request_manager_ptr->get_last_error());
+        unlock_buttons();
+        return;
+    }
+
+    // Контроль выполнения запроса.
+    if ( !handle_request(CommandType::Add)) {
+        show_message(QString("Unable to add new task!\n\n%1").arg(error_text));
+        unlock_buttons();
+        return;
+    }
+
+    // Добавляем новую задачу в коллекцию данных задач.
+    const TaskData task_data{own_id, 2, deadline, task_name, description};
+    const int new_id = collector_ptr->get_item_id();
+
+    data_keeper_ptr->add_task(new_id, task_data);
+
+    // Добавляем новую задачу в таблицу.
+    tasks_table_model->insertRow(tasks_table_model->rowCount());
+
+    const int row_number = tasks_table_model->rowCount()-1;
+
+    tasks_table_model->setData(tasks_table_model->index(row_number, 0), new_id, Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(row_number, 1), collector_ptr->status_description(2), Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(row_number, 2), task_name, Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(row_number, 3), deadline, Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(row_number, 4), description, Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(row_number, 5), QString::number(own_id), Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(row_number, 6), own_name, Qt::DisplayRole);
+
+    show_message(QString("Task %1\nhas been successfully created").arg(task_name));
+
+    unlock_buttons();
 }
 
 // Забрать чужую задачу себе.
 void UserWindow::take_task()
 {
+    // Должна быть выбрана задача.
+    const QModelIndexList selection = ui->tvTasks->selectionModel()->selectedRows();
 
+    if (selection.isEmpty()) {
+        show_message(QString("Choose a task to take"));
+        return;
+    }
+
+    bool ok = false;
+    const int task_id = tasks_table_model->item(selection.at(0).row())->data(Qt::DisplayRole).toInt(&ok);
+
+    if (!ok || task_id <= 0) {
+        return;
+    }
+
+    ok = false;
+    const int user_id = tasks_table_model->item(selection.at(0).row(), 5)->data(Qt::DisplayRole).toInt(&ok);
+
+    if (!ok || user_id < 0) {
+        return;
+    }
+
+    // Проверяем, назначена ли уже выбранная задача на текущего пользователя.
+    if (user_id == own_id) {
+        show_message(QString("This task is already appoited to you"));
+        return;
+    }
+
+    const QString task_to_take = data_keeper_ptr->get_task_data(task_id)->name;
+
+    lock_buttons();
+
+    // Отправляем запрос на удаление задачи.
+    if ( !request_manager_ptr->send_set_taskuser(task_id, own_id) ) {
+        show_message(request_manager_ptr->get_last_error());
+        unlock_buttons();
+        return;
+    }
+
+    // Контроль выполнения запроса.
+    if ( !handle_request(CommandType::Set)) {
+        show_message(QString("Unable to take task\n%1!\n\n%2")
+                         .arg(task_to_take, error_text));
+        unlock_buttons();
+        return;
+    }
+
+    // Запоминаем новое значение user_id исполнителя задачи.
+    data_keeper_ptr->set_task_user(task_id, own_id);
+
+    // Проверяем, был ли у задачи статус "not appointed".
+    if (data_keeper_ptr->get_task_data(task_id)->status == 1) {
+        // Устанавливаем статус задачи "appointed".
+        data_keeper_ptr->set_task_status(task_id, 2);
+
+        // Обновляем содержимое ячейки в таблице.
+        tasks_table_model->setData(tasks_table_model->index(selection.at(0).row(), 1),
+                                   collector_ptr->status_description(2), Qt::DisplayRole);
+
+        ui->cbTaskStatus->setCurrentIndex(1);
+    }
+
+    // Обновляем содержимое ячейки в таблице.
+    tasks_table_model->setData(tasks_table_model->index(selection.at(0).row(), 5), QString::number(own_id), Qt::DisplayRole);
+    tasks_table_model->setData(tasks_table_model->index(selection.at(0).row(), 6), own_name, Qt::DisplayRole);
+
+    show_message(QString("Task %1\nhas been successfully taken").arg(task_to_take));
+
+    unlock_buttons();
 }
 
 // Изменить статус своей задачи.
 void UserWindow::change_task_status()
 {
+    // Должна быть выбрана задача.
+    const QModelIndexList selection = ui->tvTasks->selectionModel()->selectedRows();
 
+    if (selection.isEmpty()) {
+        show_message(QString("Choose a task to change status"));
+        return;
+    }
+
+    bool ok = false;
+    const int task_id = tasks_table_model->item(selection.at(0).row())->data(Qt::DisplayRole).toInt(&ok);
+
+    if (!ok || task_id <= 0) {
+        return;
+    }
+
+    ok = false;
+    const int user_id = tasks_table_model->item(selection.at(0).row(), 5)->data(Qt::DisplayRole).toInt(&ok);
+
+    if (!ok || user_id < 0) {
+        return;
+    }
+
+    // Пользователь может изменять статус только одной из своих задач.
+    if (user_id != own_id) {
+        show_message(QString("Take task before changing status"));
+        return;
+    }
+
+    const QString task_name = data_keeper_ptr->get_task_data(task_id)->name;
+
+    const int status = ui->cbTaskStatus->currentIndex()+1;
+
+    // Пользователь не может отказаться от своей задачи.
+    if (status == 1) {
+        show_message(QString("You are not allowed to abandon the task"));
+        unlock_buttons();
+        return;
+    }
+
+    // Проверяем чтобы текущий и новый статус не совпадали.
+    if (status == data_keeper_ptr->get_task_data(task_id)->status) {
+        show_message(QString("Old and new task statuses are the same"));
+        unlock_buttons();
+        return;
+    }
+
+    lock_buttons();
+
+    // Отправляем запрос на изменение статуса задачи.
+    if ( !request_manager_ptr->send_set_taskstatus(task_id, status) ) {
+        show_message(request_manager_ptr->get_last_error());
+        unlock_buttons();
+        return;
+    }
+
+    // Контроль выполнения запроса.
+    if ( !handle_request(CommandType::Set)) {
+        show_message(QString("Unable to change status for\ntask %1!\n\n%2")
+                         .arg(task_name, error_text));
+        unlock_buttons();
+        return;
+    }
+
+    // Запоминаем новый статус задачи.
+    data_keeper_ptr->set_task_status(task_id, status);
+
+    // Обновляем содержимое ячейки в таблице.
+    tasks_table_model->setData(tasks_table_model->index(selection.at(0).row(), 1),
+                               collector_ptr->status_description(status), Qt::DisplayRole);
+
+    // Если новый статус "not appointed", присваиваем у задачи user_id = 0.
+    if ( status == 1 ) {
+        data_keeper_ptr->reset_task_user(task_id);
+
+        tasks_table_model->setData(tasks_table_model->index(selection.at(0).row(), 5), QString("0"), Qt::DisplayRole);
+        tasks_table_model->setData(tasks_table_model->index(selection.at(0).row(), 6), QString("Unknown"), Qt::DisplayRole);
+    }
+
+    show_message(QString("Task status has been successfully changed\nto %1")
+                     .arg(collector_ptr->status_description(status)));
+
+    unlock_buttons();
 }
 
 // Задать новую дату, до которой д/б выполнена своя задача.
